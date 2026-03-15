3dd7b1d60365561762de0520f4024e62ec010e4a
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2026-03-15 17:11:59 -0400

    :sparkles: Finish converting `get_provinces()` to `get_province()`

--- R/get_province.R
+++ R/get_province.R
@@ -1,21 +1,29 @@
-#' Get all provinces and territories and their holidays from the Canada Holidays API
+#' Get holidays for a province or territory from the Canada Holidays API
 #'
-#' Get all provinces and territories, and their holidays for a given year. Best used to obtain
-#' holiday information by province and territory, for all provinces and territories.
+#' Get all holidays for a given year, for a province or territory. Best used to obtain
+#' holiday information for a specific province or territory.
 #'
+#' Province and territory codes:
+#' * Alberta: `"AB"`
+#' * British Columbia: `"BC"`
+#' * Manitoba: `"MB"`
+#' * New Brunswick: `"NB"`
+#' * Newfoundland and Labrador: `"NL"`
+#' * Nova Scotia: `"NS"`
+#' * Northwest Territories: `"NT"`
+#' * Nunavut: `"NU"`
+#' * Ontario: `"ON"`
+#' * Prince Edward Island: `"PE"`
+#' * Quebec: `"QC"`
+#' * Saskatchewan: `"SK"`
+#' * Yukon: `"YT"`
+#'
+#' @param province The two letter abbreviation for a province/territory (case-sensitive).
 #' @param year The year for which holidays should be retrieved, between 2013 and 2038. The default,
 #' `NULL`, is equivalent to the current year.
 #' @param optional A boolean indicating whether optional (non-legislated) holidays should be
 #' retrieved. The default, `NULL`, is equivalent to `FALSE`.
 #' @returns A [tibble][tibble::tibble-package] with columns:
-#' \describe{
-#' \item{province_id}{`<chr>` The abbreviated province/territory code.}
-#' \item{province_name_en}{`<chr>` The name of the province/territory, in English.}
-#' \item{province_name_fr}{`<chr>` The name of the province/territory, in French.}
-#' \item{holidays}{`<list>` See below.}
-#' \item{source_info}{`<list>` A list containing a link to the information source.}
-#' }
-#' The [tibbles][tibble::tibble-package] contained inside `holidays` are:
 #' \describe{
 #' \item{date}{`<date>` The date when the holiday occurs.}
 #' \item{observed_date}{`<date>` The date when the holiday is observed (celebrated). For example,
@@ -24,16 +32,22 @@
 #' \item{name_fr}{`<chr>` The name of the holiday, in French.}
 #' \item{federal}{`<lgl>` Whether the holiday is a federal holiday.}
 #' \item{holiday_id}{`<int>` The id of the holiday.}
+#' \item{province_id}{`<chr>` The abbreviated province/territory code.}
+#' \item{province_name_en}{`<chr>` The name of the province/territory, in English.}
+#' \item{province_name_fr}{`<chr>` The name of the province/territory, in French.}
+#' \item{source_info}{`<list>` A list containing a link to the information source.}
 #' }
 #' @examples
 #' if (interactive()) {
-#'   get_provinces()
+#'   get_province()
 #' }
-#' @seealso [Canada Holidays API](https://canada-holidays.ca/)
+#' @seealso [get_holidays()], [Canada Holidays API](https://canada-holidays.ca/)
 #' @export
-get_province <- function(year = NULL, optional = NULL) {
+get_province <- function(province, year = NULL, optional = NULL) {
   # ---- Checks and type conversions ----
 
+  check_string(province)
+  check_province(province)
   check_number_whole(year, min = 2013, max = 2038, allow_null = TRUE)
   check_bool(optional, allow_null = TRUE)
 
@@ -44,7 +58,8 @@ get_province <- function(year = NULL, optional = NULL) {
 
   req <- httr2::request("https://canada-holidays.ca/api/v1") %>%
     httr2::req_user_agent("holideh R package (https://adamoshen.github.io/holideh)") %>%
-    httr2::req_url_path_append("provinces")
+    httr2::req_url_path_append("provinces") %>%
+    httr2::req_url_path_append(province)
 
   resp <- req %>%
     httr2::req_url_query(year = year, optional = optional) %>%
@@ -52,7 +67,8 @@ get_province <- function(year = NULL, optional = NULL) {
 
   result <- resp %>%
     httr2::resp_body_json(simplifyVector = TRUE) %>%
-    purrr::pluck("provinces")
+    purrr::pluck("province") %>%
+    purrr::discard_at("nextHoliday")
 
   # ---- Process results ----
 
@@ -60,8 +76,9 @@ get_province <- function(year = NULL, optional = NULL) {
     "province_id" = "id",
     "province_name_en" = "nameEn",
     "province_name_fr" = "nameFr",
-    "holidays" = "holidays",
-    "source_info" = "source_info"
+    "source_link" = "sourceLink",
+    "source_en" = "sourceEn",
+    "holidays" = "holidays"
   )
 
   inner_col_names <- c(
@@ -75,30 +92,23 @@ get_province <- function(year = NULL, optional = NULL) {
 
   result %>%
     tibble::as_tibble() %>%
-    dplyr::select(-nextHoliday) %>%
+    dplyr::rename(all_of(outer_col_names)) %>%
     tidyr::nest(source_info = tidyselect::starts_with("source")) %>%
-    dplyr::rename(tidyselect::all_of(outer_col_names)) %>%
+    tidyr::unnest(holidays) %>%
+    dplyr::rename(all_of(inner_col_names)) %>%
+    dplyr::select(
+      date, observed_date, name_en, name_fr, federal, holiday_id,
+      province_id, province_name_en, province_name_fr, source_info
+    ) %>%
     dplyr::mutate(
       province_name_fr = utf8::utf8_normalize(province_name_fr, map_compat = TRUE)
     ) %>%
     dplyr::mutate(
-      holidays = purrr::map(holidays, tibble::as_tibble),
-      holidays = purrr::map(holidays, \(x) dplyr::rename(x, tidyselect::all_of(inner_col_names))),
-      holidays = purrr::map(
-        holidays,
-        \(x) dplyr::mutate(
-          x,
-          date = lubridate::ymd(date),
-          observed_date = lubridate::ymd(observed_date),
-          name_en = utf8::utf8_normalize(name_en, map_quote = TRUE),
-          name_fr = utf8::utf8_normalize(name_fr, map_compat = TRUE, map_quote = TRUE),
-          name_fr = stringr::str_trim(name_fr),
-          federal = as.logical(federal)
-        )
-      ),
-      holidays = purrr::map(
-        holidays,
-        \(x) dplyr::select(x, date, observed_date, name_en, name_fr, federal, holiday_id)
-      )
+      date = lubridate::ymd(date),
+      observed_date = lubridate::ymd(observed_date),
+      name_en = utf8::utf8_normalize(name_en, map_quote = TRUE),
+      name_fr = utf8::utf8_normalize(name_fr, map_compat = TRUE, map_quote = TRUE),
+      name_fr = stringr::str_trim(name_fr),
+      federal = as.logical(federal)
     )
 }

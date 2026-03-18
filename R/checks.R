59223193cebe3470bff66023972889163e017b78
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2026-03-17 21:13:49 -0400

    :bento: Create additional checks

--- R/checks.R
+++ R/checks.R
@@ -12,3 +12,38 @@ check_province <- function(x, allow_null = FALSE, call = rlang::caller_env()) {
 
   cli::cli_abort("{.str {x}} is not a valid province code.", call = call)
 }
+
+check_date <- function(
+  x,
+  arg = rlang::caller_arg(x),
+  call = rlang::caller_env()
+) {
+  if (inherits(x, "Date")) {
+    return(invisible(NULL))
+  }
+
+  cli::cli_abort(
+    "{.arg {arg}} must be an object of class {.cls Date}.",
+    call = call
+  )
+}
+
+check_wdays <- function(
+  x,
+  arg = rlang::caller_arg(x),
+  call = rlang::caller_env()
+) {
+  wday_abbrs <- c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+
+  if (all(x %in% wday_abbrs)) {
+    return(invisible(NULL))
+  }
+
+  cli::cli_abort(
+    c(
+      "{.arg {arg}} contains invalid day names.",
+      "i" = "Acceptable values are one or more of: {.val {wday_abbrs}}."
+    ),
+    call = call
+  )
+}

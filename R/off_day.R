0af0354b0be90206acc32f7f792b6851527ae624
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2026-03-19 19:15:40 -0400

    :memo: Update docs for `off_day`

--- R/off_day.R
+++ R/off_day.R
@@ -4,12 +4,23 @@
 #'
 #' Holiday dates can be obtained using [get_holidays()], [get_province()], or by defining a custom
 #' vector of holidays.
+#' @name off_day
+#' @seealso [is_bizday()]
 NULL
 
 #' @param x A vector of dates or date-times. If date-times are supplied, the date component will be
 #' extracted.
 #' @param holidays A vector of dates that are holidays.
-#' @returns A named logical vector of length equal to `x`.
+#' @returns A logical vector of length equal to `x`.
+#' @rdname off_day
+#' @examples
+#' library(lubridate)
+#'
+#' dates <- seq.Date(from = ymd("2025-12-20"), to = ymd("2025-12-31"), by = "1 day")
+#' winter_holidays <- ymd(c("2025-12-25", "2025-12-26"))
+#'
+#' rlang::set_names(is_holiday(dates, holidays = winter_holidays), dates)
+#' rlang::set_names(is_weekend(dates), dates)
 #' @export
 is_holiday <- function(x, holidays) {
   # If x is a datetime, extract the date component
@@ -25,6 +36,7 @@ is_holiday <- function(x, holidays) {
 #' @param weekend A character vector of three-letter abbreviations of weekday names indicating days
 #' that should be considered a weekend. Acceptable values are: `"Sun"`, `"Mon"`, `"Tue"`, `"Wed"`,
 #' `"Thu"`, `"Fri"`, `"Sat"`, `"Sun"`.
+#' @rdname off_day
 #' @export
 is_weekend <- function(x, weekend = c("Sat", "Sun")) {
   lubridate::wday(x, label = TRUE) %in% weekend
